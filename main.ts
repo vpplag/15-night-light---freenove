af25767c9dfa54efb9c1e5863c1249806f09cd89
vpplag <74141971+vpplag@users.noreply.github.com>
2021-04-12 00:39:57 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,7 @@
 basic.forever(function () {
-    if (pins.analogReadPin(AnalogPin.P1) > 400) {
+    if (pins.analogReadPin(AnalogPin.P1) < 400) {
         pins.digitalWritePin(DigitalPin.P0, 0)
     } else {
         pins.digitalWritePin(DigitalPin.P0, 1)
     }
-    serial.writeNumber(pins.analogReadPin(AnalogPin.P1))
 })
